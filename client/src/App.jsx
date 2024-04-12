ab42e94dcdfb0f77d3803a53355f63ccaca86648
alexis.guillon <alexis.guillon44@orange.fr>
2024-04-12 13:48:58 +0200

    NavBar and css architecure is agree

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,32 @@
 import "./App.css";
-import { Outlet, Link } from "react-router-dom"
-
+import { Outlet, Link } from "react-router-dom";
+import greenDoorPot from "./assets/images/greenDoorPot.svg";
+import "./styles/Import.css";
 
 function App() {
   return (
     <>
       <nav>
         <ul>
+          <button type="button">
+            <img src={greenDoorPot} alt="Logo greendoor" />
+          </button>
+          <h1>
+            greenDoors
+            
+          </h1>
+
           <li>
             <Link to='/'>Home</Link>
           </li>
           <li>
-            <Link to="quizz">Quizz</Link>
+            <Link to="quizz">Plant Quizz</Link>
           </li>
           <li>
             <Link to="myplants">My Plants</Link>
           </li>
         </ul>
       </nav>
-      <h1>
-        GREENDOOR
-
-      </h1>
 
       <Outlet />
     </>
