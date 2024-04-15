b7816f3ee5033b6c42ef1a7fab3a826732f6d2d5
AkiraAcetonoshi <facilitateurmaker@gmail.com>
2024-04-15 21:26:21 +0200

    Duplicated Explicit Filter component for sunlight

--- client/src/components/FiltersTutorial.jsx
+++ client/src/components/FiltersTutorial.jsx
@@ -2,13 +2,17 @@ import { useState } from "react";
 import ExplicitFilter from "./ExplicitFilter";
 import waterIconInactive from "../assets/icons/WaterGrey.png";
 import waterIconActive from "../assets/icons/WaterBlue.png";
+import lightIconInactive from "../assets/icons/SunGrey.png";
+import lightIconActive from "../assets/icons/SunYellow.png";
 
 
 function FiltersTutorial() {
   // these filters can have three values : 0,1 and 2.   
   const [waterFilter, setWaterFilter] = useState(1);
+  const [lightFilter, setLightFilter] = useState(1);
   // TODO : Ask Theo if he's okay to change the desciptions with "water me every now and then", "i need to be watered regularly","i need to be watered every couple of days" or smth
   const waterFiltersDescriptions=["Little need for water","Average need for water","High need for water"]
+  const lightFiltersDescriptions=["Can withstand poorly lit environnement","Needs a relatively well lit environement","High need for sunlight"]
 
 
   return (
@@ -21,6 +25,14 @@ function FiltersTutorial() {
         iconInactive={waterIconInactive}
         filter={waterFilter}
         setFilter={setWaterFilter}
+      />
+      <ExplicitFilter
+        title="Sunlight"
+        filtersDescriptions={lightFiltersDescriptions}
+        iconActive={lightIconActive}
+        iconInactive={lightIconInactive}
+        filter={lightFilter}
+        setFilter={setLightFilter}
       />
     </section>
   );
