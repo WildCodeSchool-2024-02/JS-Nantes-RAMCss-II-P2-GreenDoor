d75900dbfc66ab496aef4d74658fee23c975b466
AkiraAcetonoshi <facilitateurmaker@gmail.com>
2024-04-15 18:54:01 +0200

    added most of the icons and centered the image within the Header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,17 +1,22 @@
-import '../styles/Header.css'
+import "../styles/Header.css";
 
 function Header() {
   return (
     <header>
       <h1>Find the perfect plants for your interior</h1>
       <h2>and learn to take good care of them</h2>
-      <input
-        type="search"
-        name="search-form"
-        id="search-form"
-        className="search-input"
-        placeholder="Search for a plant"
-      />
+      <form>
+        <label htmlFor="search-form" >
+          <img src="../assets/icons/SearchIcon.png" alt="Search Icon" />
+        </label>
+        <input
+          type="search"
+          name="search-form"
+          id="search-form"
+          className="search-input"
+          placeholder="Search for a plant"
+        />
+      </form>
     </header>
   );
 }
