4555d005c0a915708506cb461ce1c12a4cd68e3a
AkiraAcetonoshi <facilitateurmaker@gmail.com>
2024-04-15 17:38:44 +0200

    started to style the header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,19 @@
+import '../styles/Header.css'
+
 function Header() {
-    return(
-        <header>
-            <h1>Find the perfect plants for your interior</h1>
-            <h2>and learn to take good care of them</h2>
-        </header>
-    )
+  return (
+    <header>
+      <h1>Find the perfect plants for your interior</h1>
+      <h2>and learn to take good care of them</h2>
+      <input
+        type="search"
+        name="search-form"
+        id="search-form"
+        className="search-input"
+        placeholder="Search for a plant"
+      />
+    </header>
+  );
 }
 
-export default Header
+export default Header;
