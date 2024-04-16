ae12664d47572e8d0e910010c8cf1509949b9863
AkiraAcetonoshi <facilitateurmaker@gmail.com>
2024-04-16 17:21:33 +0200

    added Quizz component and made some CSS to split the page in two

--- client/src/pages/Quizz.jsx
+++ client/src/pages/Quizz.jsx
@@ -1,9 +1,31 @@
-function Quizz (){
-    return (
-        <main>
-            Quizz ici
-        </main>
-    )
+import background from "../assets/images/headerCactus.jpg";
+import "../styles/Quizz.css";
+
+function Quizz() {
+  return (
+    <section className="full-page-container">
+      <figure>
+        <img src={background} alt="decorative" />
+      </figure>
+      <section className="right-container">
+        <p>Question</p>
+        <ul className="box">
+          <li className="box 1">
+            <p>box 1</p>
+          </li>
+          <li className="box2">
+            <p>box 2</p>
+          </li>
+          <li className="box3">
+            <p>box 3</p>
+          </li>
+          <li className="box4">
+            <p>box 4</p>
+          </li>
+        </ul>
+      </section>
+    </section>
+  );
 }
 
-export default Quizz
+export default Quizz;
