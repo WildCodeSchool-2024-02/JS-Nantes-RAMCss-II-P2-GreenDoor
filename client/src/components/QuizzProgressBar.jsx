43bc7e4aa099982c376252c1df78c209b9374eca
AkiraAcetonoshi <facilitateurmaker@gmail.com>
2024-04-16 19:15:43 +0200

    removed the margin-top, it was a mistake and added a progress bar to the quizz, as well a a map for the buttons, they have a class that differs based on how many buttons there are

---
+++ client/src/components/QuizzProgressBar.jsx
@@ -0,0 +1,13 @@
+import PropTypes from "prop-types";
+
+function QuizzProgressBar({ topic, questionIndex }) {
+
+  return <p>{`progress bar ${topic} and ${questionIndex}`}</p>;
+}
+
+QuizzProgressBar.propTypes = {
+    topic: PropTypes.string.isRequired,
+    questionIndex: PropTypes.number.isRequired,
+  };
+
+export default QuizzProgressBar;
--- client/src/pages/Quizz.jsx
+++ client/src/pages/Quizz.jsx
@@ -1,27 +1,46 @@
+import { useState } from "react";
+import QuizzProgressBar from "../components/QuizzProgressBar";
+import quizz from "./quizz.json";
 import background from "../assets/images/headerCactus.jpg";
 import "../styles/Quizz.css";
 
 function Quizz() {
+  const [questionIndex, setQuestionIndex] = useState(0);
+  const [answears, setAnswears] = useState([]);
+
+
+  function handleQuizzButton(index) {
+    setQuestionIndex(() => questionIndex + 1);
+    setAnswears(() => {
+        const updatedAnswears = [...answears]
+        updatedAnswears.push(index);
+        return updatedAnswears;
+    });
+  }
+
   return (
     <section className="full-page-container">
       <figure>
         <img src={background} alt="decorative" />
       </figure>
       <section className="right-container">
-        <p>Question</p>
-        <ul className="box">
-          <li className="box 1">
-            <p>box 1</p>
-          </li>
-          <li className="box2">
-            <p>box 2</p>
-          </li>
-          <li className="box3">
-            <p>box 3</p>
-          </li>
-          <li className="box4">
-            <p>box 4</p>
-          </li>
+        <QuizzProgressBar
+          topic={quizz[questionIndex]}
+          questionIndex={questionIndex}
+        />
+        <h2>{quizz[questionIndex].question}</h2>
+        <ul>
+          {quizz[questionIndex].answears.map((answear, index) => (
+            <li key={answear}>
+              <button
+                className={`layout-${quizz[questionIndex].answears.length}`}
+                type="button"
+                onClick={() => handleQuizzButton(index)}
+              >
+                {answear}
+              </button>
+            </li>
+          ))}
         </ul>
       </section>
     </section>
