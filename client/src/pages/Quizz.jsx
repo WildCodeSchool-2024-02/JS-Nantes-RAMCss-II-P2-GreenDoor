c201b1ab82737873fe226701828f2832162033ec
AkiraAcetonoshi <facilitateurmaker@gmail.com>
2024-04-17 12:51:35 +0200

    made the intro page for the quizz

--- client/src/pages/Quizz.jsx
+++ client/src/pages/Quizz.jsx
@@ -5,7 +5,8 @@ import background from "../assets/images/headerCactus.jpg";
 import "../styles/Quizz.css";
 
 function Quizz() {
-  const [questionIndex, setQuestionIndex] = useState(0);
+  // this state is used to keep track of the question index, and also to display the intro page if it's null
+  const [questionIndex, setQuestionIndex] = useState(null);
   const [answears, setAnswears] = useState([]);
 
   function handleQuizzButton(index) {
@@ -23,20 +24,42 @@ function Quizz() {
         <img src={background} alt="decorative" />
       </figure>
       <section className="questions-container">
-        <QuizzProgressBar quizz={quizz} questionIndex={questionIndex} />
-        <h2>{quizz[questionIndex].question}</h2>
+        {questionIndex === null ? (
+          // This is the intro of the quizz
+          <>
+            <h1>
+              Find your <strong>dream plant</strong> !
+            </h1>
+            <h3>
+              Take a 1-minute assessment to tell us about yourself and enable us
+              to identify the ideal house plant that matches your preferences
+            </h3>
+            <button type="button" onClick={() => setQuestionIndex(() => 0)}>
+              Start the quizz
+            </button>
+          </>
+        ) : (
+          // this is the part with the regular quizz
+          <>
+            <QuizzProgressBar quizz={quizz} questionIndex={questionIndex} />
+            <h2>{quizz[questionIndex].question}</h2>
 
-        <ul
-          className={`layout-${quizz[questionIndex].answears.length} buttons-container`}
-        >
-          {quizz[questionIndex].answears.map((answear, index) => (
-            <li key={answear}>
-              <button type="button" onClick={() => handleQuizzButton(index)}>
-                {answear}
-              </button>
-            </li>
-          ))}
-        </ul>
+            <ul
+              className={`layout-${quizz[questionIndex].answears.length} buttons-container`}
+            >
+              {quizz[questionIndex].answears.map((answear, index) => (
+                <li key={answear}>
+                  <button
+                    type="button"
+                    onClick={() => handleQuizzButton(index)}
+                  >
+                    {answear}
+                  </button>
+                </li>
+              ))}
+            </ul>
+          </>
+        )}
       </section>
     </section>
   );
