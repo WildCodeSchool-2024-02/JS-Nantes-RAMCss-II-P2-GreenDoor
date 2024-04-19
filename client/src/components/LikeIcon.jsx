3776ce86578b83d797fe22440c8ea2714a7e12c4
TheoDegorre <degorret@gmail.com>
2024-04-19 10:33:23 +0200

    feature like

--- client/src/components/LikeIcon.jsx
+++ client/src/components/LikeIcon.jsx
@@ -1,11 +1,15 @@
+// import "../styles/LikeIcon.css";
 // import { useState } from "react";
 
-function LikeIcon() {
-  return (
-    <div>
-      <p className="Truc">❣️</p>
-    </div>
-  );
-}
+// function LikeIcon() {
+//   const [like, setlike] = useState(false);
 
-export default LikeIcon;
+//   return (
+//     <div>
+//       <button className = "click-like-icon" type="button" onClick ={() => setlike((like) => true)}>Like</button>
+//       <p className="truc">❣️</p>
+//     </div>
+//   );
+// }
+
+// export default LikeIcon;
