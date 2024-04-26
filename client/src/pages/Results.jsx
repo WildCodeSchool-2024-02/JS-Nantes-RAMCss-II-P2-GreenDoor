a707c8b41c0abc841ad43f36722b394c8c7ffd16
alexis.guillon <alexis.guillon44@orange.fr>
2024-04-26 12:17:56 +0200

    US_11_spinner is functional and favicon is ready

--- client/src/pages/Results.jsx
+++ client/src/pages/Results.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import PlantCards from "../components/PlantCards";
 import Footer from "../components/Footer";
+import Spinner from "../components/Spinner";
 import plants from "../plants.json";
 import waterIconInactive from "../assets/icons/WaterGrey.png";
 import waterIconActive from "../assets/icons/WaterBlue.png";
@@ -10,95 +11,90 @@ import lightIconActive from "../assets/icons/SunYellow.png";
 
 function Results() {
   const { answers } = useParams();
-  const [suitablePlants, setSuitablePlants] = useState(plants);
+  const [suitablePlants, setSuitablePlants] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-  // this hook will trigger an effect at page loadup
   useEffect(() => {
-    setSuitablePlants(() =>
-      plants.filter((plant) => {
-        let plantFitsCriteria = true;
+    setLoading(true); // Launch the spinner
 
-        // toxicity : ignore for now
-        // const toxicityCriteria = answers[1];
+    const filteredPlants = plants.filter((plant) => {
+      let plantFitsCriteria = true;
 
-        // Growth `["Yes, i want to see it grow", "No, i prefer that it stays the same", "Skip question"]`
-        // const growthCriteria = answers[2];
+      const careCriteria = answers[0];
+      if (
+        careCriteria >= 2 &&
+        (plant.watering.includes("Water when soil is half dry") ||
+          plant.watering.includes("Must not dry between watering"))
+      ) {
+        plantFitsCriteria = false;
+      } else if (
+        careCriteria >= 1 &&
+        plant.watering.includes("Must not dry between watering")
+      ) {
+        plantFitsCriteria = false;
+      }
 
-        // first approach : return false if one of the criteria doesn't match
+      const roomOrientationCriteria = answers[3];
+      if (
+        (roomOrientationCriteria === "0" || roomOrientationCriteria === "2") &&
+        plant.lightIdeal.includes("Full sun")
+      ) {
+        plantFitsCriteria = false;
+      } else if (
+        roomOrientationCriteria === "1" &&
+        (plant.lightTolered ? plant.lightTolered.includes("Diffuse") : true)
+      ) {
+        plantFitsCriteria = false;
+        // for south oriented plants, and those that tolerate only "strong" light conditions
+      }
 
-        // treat care levels `["Once a day", "Once a week", "Once a month", "Once a year"]`
-        const careCriteria = answers[0];
+      return plantFitsCriteria;
+    });
 
-        // if you want to water yout plants less often than once a month, exclude those that cannot dry
-        if (
-          careCriteria >= 2 &&
-          (plant.watering.includes("Water when soil is half dry") ||
-            plant.watering.includes("Must not dry between watering"))
-        ) {
-          plantFitsCriteria = false;
-          // if you want to water yout plants once a week at max, exclude those that cannot dry
-        } else if (
-          careCriteria >= 1 &&
-          plant.watering.includes("Must not dry between watering")
-        ) {
-          plantFitsCriteria = false;
-        }
-
-        // Room orientation [0 : North, 1 : South, 2 : East, 3 West]
-        const roomOrientationCriteria = answers[3];
-        // Sunlight `["I'm drowning in sunlight","A little bit of light","I'm a vampire","Skip question"]`
-        // const sunlightCriteria = answers[4];
-
-        // For north or east-oriented rooms, whatever the light they get
-        if (
-          (roomOrientationCriteria === "0" ||
-            roomOrientationCriteria === "2") &&
-          plant.lightIdeal.includes("Full sun")
-        ) {
-          plantFitsCriteria = false;
-          // for south oriented plants, and those that tolerate only "strong" light conditions
-        } else if (
-          roomOrientationCriteria === "1" &&
-          (plant.lightTolered ? plant.lightTolered.includes("Diffuse") : true)
-        ) {
-          plantFitsCriteria = false;
-        }
-
-        return plantFitsCriteria;
-      })
-    );
+    setTimeout(() => {
+      setSuitablePlants(filteredPlants);
+      setLoading(false); // Stop the spinner
+    }, 5000);
   }, [answers]);
 
   return (
     <>
       <hgroup className="results-header">
-        <h1>Here are the plants that match your criteria</h1>
+        <h1>
+          {loading
+            ? "We are actively searching the best of the best plants for you"
+            : "Here are the plants that match your criteria"}
+        </h1>
       </hgroup>
-      <section className="compact-cards-container">
-        {suitablePlants.map((el) => (
-          <PlantCards
-            key={el.id}
-            img={el.img}
-            commonName={el.commonName}
-            latinName={el.latinName}
-            watering={el.watering}
-            growth={el.growth}
-            heightPotential={el.heightPotential.CM}
-            pruning={el.pruning}
-            temperatureMax={el.temperatureMax.C}
-            temperatureMin={el.temperatureMin.C}
-            disease={el.disease}
-            insects={el.insects}
-            waterIconActive={waterIconActive}
-            waterIconInactive={waterIconInactive}
-            lightIconActive={lightIconActive}
-            lightIconInactive={lightIconInactive}
-            lightIdeal={el.lightIdeal}
-            lightTolered={el.lightTolered}
-            compactDisplay
-          />
-        ))}
-      </section>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <section className="compact-cards-container">
+          {suitablePlants.map((el) => (
+            <PlantCards
+              key={el.id}
+              img={el.img}
+              commonName={el.commonName}
+              latinName={el.latinName}
+              watering={el.watering}
+              growth={el.growth}
+              heightPotential={el.heightPotential.CM}
+              pruning={el.pruning}
+              temperatureMax={el.temperatureMax.C}
+              temperatureMin={el.temperatureMin.C}
+              disease={el.disease}
+              insects={el.insects}
+              waterIconActive={waterIconActive}
+              waterIconInactive={waterIconInactive}
+              lightIconActive={lightIconActive}
+              lightIconInactive={lightIconInactive}
+              lightIdeal={el.lightIdeal}
+              lightTolered={el.lightTolered}
+              compactDisplay
+            />
+          ))}
+        </section>
+      )}
       <Footer />
     </>
   );
