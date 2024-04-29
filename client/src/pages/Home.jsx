70ac89a3f576fcf8e07a9e232ebf8fd55ccfb8f1
Micka972 <dem972md@gmail.com>
2024-04-29 16:31:35 +0200

    css responsive en cours

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -16,7 +16,6 @@ function Home() {
   const [waterFilter, setWaterFilter] = useState(1);
   const [lightFilter, setLightFilter] = useState(1);
 
-
   const [search, setSearch] = useState("");
   const [filteredPlant, setFilteredPlant] = useState([]);
 
